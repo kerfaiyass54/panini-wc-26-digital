bcc711c5f7ba8aefb385ba163cefa8e0d1dc226a
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-05-16 16:50:14 +0100

    Added total and etails pages

--- panini-wc-ui/src/app/stickers-total/stickers-total.ts
+++ panini-wc-ui/src/app/stickers-total/stickers-total.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-stickers-total',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './stickers-total.html',
   styleUrl: './stickers-total.scss',
 })
-export class StickersTotal {}
+export class StickersTotal {
+
+  // TOTAL STICKERS
+
+  totalStickers = signal(684);
+
+  collected = signal(472);
+
+  // CATEGORIES
+
+  logos = signal(32);
+
+  introPictures = signal(18);
+
+  players = signal(422);
+
+  // PERCENTAGE
+
+  percentage = computed(() =>
+
+    Math.round(
+      (this.collected() / this.totalStickers()) * 100
+    )
+  );
+}
