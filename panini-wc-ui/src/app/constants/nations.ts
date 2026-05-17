81272b677e9762b713019e30c7fb9aea50b94329
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-05-17 17:15:24 +0100

    Added constant for nations

---
+++ panini-wc-ui/src/app/constants/nations.ts
@@ -0,0 +1,12 @@
+export const nations: string[] = [
+  "Algeria", "Argentina", "Australia", "Austria", "Belgium",
+  "Bosnia and Herzegovina", "Brazil", "Canada", "Cape Verde", "Colombia",
+  "Congo DR", "Croatia", "Curaçao", "Czechia", "Ecuador",
+  "Egypt", "England", "France", "Germany", "Ghana",
+  "Haiti", "Iran", "Iraq", "Ivory Coast", "Japan",
+  "Jordan", "Mexico", "Morocco", "Netherlands", "New Zealand",
+  "Norway", "Panama", "Paraguay", "Portugal", "Qatar",
+  "Saudi Arabia", "Scotland", "Senegal", "South Africa", "Spain",
+  "Sweden", "Switzerland", "Tunisia", "Turkey", "United States",
+  "Uruguay", "Uzbekistan"
+];
